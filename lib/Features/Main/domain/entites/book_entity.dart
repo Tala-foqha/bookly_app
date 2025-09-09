2c1a59f63460d472ef2f670725d7ede2d5ccc975
tala <foqhat835@gmail.com>
2025-09-09 13:55:43 +0300

    fetch newest use case

--- lib/Features/Main/domain/entites/book_entity.dart
+++ lib/Features/Main/domain/entites/book_entity.dart
@@ -2,7 +2,10 @@
 class BookEntity{
   final String image;
   final String title;
-  final String description;
+  final String author;
+  final num rating;
+  final num price;
 
-  BookEntity({required this.image, required this.title, required this.description});
+  BookEntity({required this.image, required this.title, required this.author, required this.rating, required this.price});
+
 }
--- lib/Features/Main/domain/use_case/fetch_newest_books_use_case.dart
+++ lib/Features/Main/domain/use_case/fetch_newest_books_use_case.dart
@@ -1,19 +1,19 @@
-// Features/Main/domain/use_case/fetch_featured_books_use_case.dart
+// Features/Main/domain/use_case/fetch_newest_books_use_case.dart
 import 'package:bookly_app/Features/Main/domain/entites/book_entity.dart';
 import 'package:bookly_app/Features/Main/domain/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase extends UseCase< List<BookEntity>,NoParam> {
+class FetchNewestBooksUseCase extends UseCase< List<BookEntity>,NoParam> {
   final HomeRepo homeRepo;
 
-  FetchFeaturedBooksUseCase({required this.homeRepo});
+  FetchNewestBooksUseCase({required this.homeRepo});
 
 
      
       @override
       Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) {
-     return homeRepo.fetchFeaturedBooks();
+     return homeRepo.fetchNewestBooks();
       }
 
 }
