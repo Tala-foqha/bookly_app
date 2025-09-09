44fa9770bc8380d8598be8ba5764d2891856b76c
tala <foqhat835@gmail.com>
2025-09-09 10:01:12 +0300

    book entity done

---
+++ lib/Features/Main/domain/entites/book_entity.dart
@@ -0,0 +1,8 @@
+// Features/Main/domain/entites/book_entity.dart
+class BookEntity{
+  final String image;
+  final String title;
+  final String description;
+
+  BookEntity({required this.image, required this.title, required this.description});
+}
