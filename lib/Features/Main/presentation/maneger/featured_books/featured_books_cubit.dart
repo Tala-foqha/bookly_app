107ad0e0f9f7785b2660cd1eeab15158dbbb192d
tala <foqhat835@gmail.com>
2025-09-10 15:26:42 +0300

    create featured books cubit

--- lib/Features/Main/presentation/maneger/featured_books/featured_books_cubit.dart
+++ lib/Features/Main/presentation/maneger/featured_books/featured_books_cubit.dart
@@ -1,11 +1,20 @@
 // Features/Main/presentation/maneger/featured_books/featured_books_cubit.dart
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/Main/domain/entites/book_entity.dart';
+import 'package:bookly_app/Features/Main/domain/use_case/fetch_featured_books_use_case.dart';
 import 'package:meta/meta.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.featuredBooksUseCase) : super(FeaturedBooksInitial());
+final FetchFeaturedBooksUseCase featuredBooksUseCase;
+  Future<void>fetchFeaturedBooks()async{
+    emit(FeaturedBooksLoading());
+   var result=await featuredBooksUseCase.call();
+result.fold((f)=>emit(FeaturedBooksFailure(errorMessage: f.toString())),
+ (s)=>emit(FeaturedBooksSuccess(books: s)));
+  }
 
   
 }
--- lib/Features/Main/presentation/maneger/featured_books/featured_books_state.dart
+++ lib/Features/Main/presentation/maneger/featured_books/featured_books_state.dart
@@ -6,7 +6,11 @@ abstract class FeaturedBooksState {}
 
 class FeaturedBooksInitial extends FeaturedBooksState {}
 class FeaturedBooksLoading extends FeaturedBooksState {}
-class FeaturedBooksSuccess extends FeaturedBooksState {}
+class FeaturedBooksSuccess extends FeaturedBooksState {
+final   List<BookEntity>books;
+
+  FeaturedBooksSuccess({required this.books});
+}
 class FeaturedBooksFailure extends FeaturedBooksState {
   final String errorMessage;
 
